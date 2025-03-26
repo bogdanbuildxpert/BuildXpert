f1c05e235677d375270247298cf1db2f965b6808
bogdanhutuleac <bogdanhutuleac@outlook.com>
2025-03-26 16:35:54 +0000

    fix 2gb of memory

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,14 +7,14 @@ module.exports = {
       exec_mode: "fork",
       autorestart: true,
       watch: false,
-      max_memory_restart: "1G",
+      max_memory_restart: "1.5G",
       env: {
         NODE_ENV: "production",
         PORT: 3000,
         NEXTAUTH_URL: "https://buildxpert.ie",
         NEXTAUTH_SECRET:
           process.env.NEXTAUTH_SECRET || "your-development-secret-key",
-        NODE_OPTIONS: "--max-old-space-size=768",
+        NODE_OPTIONS: "--max-old-space-size=1536",
       },
       error_file: "logs/pm2-error.log",
       out_file: "logs/pm2-out.log",

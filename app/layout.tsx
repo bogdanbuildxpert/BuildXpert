870c8ea54c672cee0daec04a54ef458deb8aa839
bogdanhutuleac <bogdanhutuleac@outlook.com>
2025-03-31 22:42:51 +0100

    Add Google Analytics configuration with environment variable

--- app/layout.tsx
+++ app/layout.tsx
@@ -147,14 +147,14 @@ export default function RootLayout({
         {/* Global Site Tag - Google Analytics */}
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-MEASUREMENT_ID`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-MEASUREMENT_ID');
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
           `}
         </Script>
 
